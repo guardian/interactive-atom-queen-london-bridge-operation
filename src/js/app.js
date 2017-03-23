0578ce91946737cb71dbcbf376df262df3d97d77
cfen <chris.fenn@guardian.co.uk>
2017-03-23 15:06:25 +0000

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+document.getElementById("headThree").className += " ani-three";
+document.getElementById("headTwo").className += " ani-two";
+document.getElementById("headOne").className += " ani-one";
